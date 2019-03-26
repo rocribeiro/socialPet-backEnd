2e32d83664578213514ef7cdeca009db72548224
Rodrigo Ribeiro <rodrigo.ribeiro03@hotmail.com>
2019-03-26 09:13:22 -0300

    Dependencias primarias maven

---
+++ src/main/java/com/social/pet/socialPet/SocialPetApplication.java
@@ -0,0 +1,13 @@
+package com.social.pet.socialPet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SocialPetApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SocialPetApplication.class, args);
+	}
+
+}
