9ea9bf80bcacbfdd6868321ce2cd6b7aa249c2a7
Rodrigo Ribeiro <rodrigo.ribeiro03@hotmail.com>
2019-09-19 23:56:17 -0300

    ajustes

--- src/main/java/com/social/pet/socialPet/controller/PetController.java
+++ src/main/java/com/social/pet/socialPet/controller/PetController.java
@@ -2,6 +2,7 @@ package com.social.pet.socialPet.controller;
 
         import com.social.pet.socialPet.model.Pet;
         import com.social.pet.socialPet.service.PetService;
+        import org.apache.coyote.Response;
         import org.springframework.beans.factory.annotation.Autowired;
         import org.springframework.http.HttpStatus;
         import org.springframework.stereotype.Controller;
@@ -15,10 +16,10 @@ public class PetController {
     @Autowired
     private PetService petService;
     @PostMapping("/addPet")
-    @ResponseStatus(HttpStatus.OK)
-    public void AdcionarAnimal(@RequestBody Pet pet){
+    public @ResponseBody String AdcionarAnimal(@RequestBody Pet pet){
 
         petService.addPet(pet);
+        return "200";
     }
     @GetMapping("/")
     public @ResponseBody List<Pet> buscaPetsPerdidos(){
